c8a66a48730378ef11a2f9348b71ea6091f528cb
Aleksey Dmitriev <ad@blockchaininstitute.io>
2018-11-09 15:12:32 +0300

    Init VK connect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import connect from '@vkontakte/vkui-connect';
 
 import App from './App';
+
+connect.send('VKWebAppInit', {});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
