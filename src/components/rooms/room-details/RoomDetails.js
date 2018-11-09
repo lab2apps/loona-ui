26b6238fe1c5d9bb068dabf3a57216798f6c912f
Bullwinkle <developer085@gmail.com>
2018-11-09 22:18:12 +0300

    spaces, rooms -list-items and -details dummy components

---
+++ src/components/rooms/room-details/RoomDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Div,
+} from '@vkontakte/vkui';
+
+type RoomDetailsProps = {
+}
+
+export class RoomDetails extends React.PureComponent<RoomDetailsProps> {
+  render () {
+    return (
+      <Div>
+        Room details
+      </Div>
+    );
+  }
+}
---
+++ src/components/spaces/space-details/SpaceDetails.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Div,
+} from '@vkontakte/vkui';
+
+
+type SpaceDetailsProps = {
+}
+
+export class SpaceDetails extends React.PureComponent<SpaceDetailsProps> {
+  render () {
+    return (
+      <Div>
+        Space details
+      </Div>
+    );
+  }
+}
