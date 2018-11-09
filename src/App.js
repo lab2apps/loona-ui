e33b8ebcccebf28420c0b1810b3e7c819b2d0d78
Bullwinkle <developer085@gmail.com>
2018-11-09 18:19:46 +0300

    debug router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 
 import { Main } from './components/pages/main/Main';
 
 import 'styles/app.scss';
+import { EditRoomForm } from './components/rooms/edit-room-form/EditRoomForm';
+import { EditSpaceForm } from './components/spaces/edit-space-form/EditSpaceForm';
 
 
 
 export class App extends Component {
   render () {
     return (
-      <Switch>
-        <Route path='/' exact={ true } component={ Main }/>
-      </Switch>
+      <div>
+        {/* DEBUG ROUTER */}
+        <div><Link to="/edit-room">Main</Link></div>
+        <div><Link to="/edit-room">EditRoomForm</Link></div>
+        <div><Link to="/edit-space">EditSpaceForm</Link></div>
+
+        <Switch>
+          <Route path='/' exact={ true } component={ Main }/>
+          <Route path='/edit-room' exact={ true } component={ EditRoomForm }/>
+          <Route path='/edit-space' exact={ true } component={ EditSpaceForm }/>
+        </Switch>
+      </div>
     );
   }
 }
