32320e7366c0551b2f8d500cfcc882cab058c3d0
Bullwinkle <developer085@gmail.com>
2018-11-09 19:21:34 +0300

    create/edit forms for room and space

--- src/components/spaces/edit-space-form/EditSpaceForm.js
+++ src/components/spaces/edit-space-form/EditSpaceForm.js
@@ -5,45 +5,38 @@ import {
   FormLayout,
   FormLayoutGroup,
   Input, Link,
-  Panel,
-  PanelHeader,
   Radio,
   Select,
-  Textarea, View,
+  Textarea,
 } from '@vkontakte/vkui';
 
 export class EditSpaceForm extends React.PureComponent {
   render () {
     return (
-      <View activePanel="new-user">
-        <Panel id="new-user" theme="white">
-          <PanelHeader>Регистрация</PanelHeader>
-          <FormLayout>
-            <Input type="email" top="E-mail" />
-            <FormLayoutGroup top="Пароль" bottom="Пароль может содержать только латинские буквы и цифры.">
-              <Input type="password"  placeholder="Введите пароль" />
-              <Input type="password" placeholder="Повторите пароль" />
-            </FormLayoutGroup>
-            <Input top="Имя" />
-            <Input top="Фамилия" />
-            <Select top="Пол" placeholder="Выберите пол">
-              <option value="m">Мужской</option>
-              <option value="f">Женский</option>
-            </Select>
-            <div top="Тип документа">
-              <Radio name="type">Паспорт</Radio>
-              <Radio name="type">Загран</Radio>
-            </div>
-            <Textarea top="О себе" />
-            <FormLayoutGroup>
-              <Checkbox>Согласен со всем <Link>этим</Link></Checkbox>
-              <div>
-                <Button type="cell">Зарегистрироваться</Button>
-              </div>
-            </FormLayoutGroup>
-          </FormLayout>
-        </Panel>
-      </View>
+      <FormLayout>
+        <Input type="email" top="E-mail" />
+        <FormLayoutGroup top="Пароль" bottom="Пароль может содержать только латинские буквы и цифры.">
+          <Input type="password"  placeholder="Введите пароль" />
+          <Input type="password" placeholder="Повторите пароль" />
+        </FormLayoutGroup>
+        <Input top="Имя" />
+        <Input top="Фамилия" />
+        <Select top="Пол" placeholder="Выберите пол">
+          <option value="m">Мужской</option>
+          <option value="f">Женский</option>
+        </Select>
+        <div top="Тип документа">
+          <Radio name="type">Паспорт</Radio>
+          <Radio name="type">Загран</Radio>
+        </div>
+        <Textarea top="О себе" />
+        <FormLayoutGroup>
+          <Checkbox>Согласен со всем <Link>этим</Link></Checkbox>
+          <div>
+            <Button type="cell">Зарегистрироваться</Button>
+          </div>
+        </FormLayoutGroup>
+      </FormLayout>
     );
   }
 }
