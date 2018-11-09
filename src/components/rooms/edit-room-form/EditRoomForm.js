612686354e976157bc8b166d420855cc2ac7dccb
Bullwinkle <developer085@gmail.com>
2018-11-09 17:44:38 +0300

    base edit forms

---
+++ src/components/rooms/edit-room-form/EditRoomForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class EditRoomForm extends React.PureComponent {
+  render () {
+    return (
+      <div className='l-main'>
+        EDIT ROOM
+      </div>
+    );
+  }
+}
---
+++ src/components/spaces/edit-space-form/EditSpaceForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class EditSpaceForm extends React.PureComponent {
+  render () {
+    return (
+      <div className='l-main'>
+        EDIT SPACE
+      </div>
+    );
+  }
+}
