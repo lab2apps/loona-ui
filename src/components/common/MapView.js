17bad291200f1633d35c8653bd17bcad0d1f65d4
Bullwinkle <developer085@gmail.com>
2018-11-10 14:55:04 +0300

    map marker, get current position

--- src/components/common/MapView.js
+++ src/components/common/MapView.js
@@ -1,27 +1,53 @@
 import React from 'react';
-import { YMaps as YMapsContainer, Map as YMap } from 'react-yandex-maps';
+import { YMaps as YMapsContainer, Map as YMap, Placemark } from 'react-yandex-maps';
 
-
-type MavViewProps = {
-}
+type MavViewProps = {}
 
 export class MapView extends React.PureComponent<MavViewProps> {
 
-  mapState = {
-    center: [55.75, 37.57],
-    zoom: 9
+  state = {
+    mapOptions: {
+      center: [55.75, 37.57],
+      zoom: 9,
+    },
+    currentCoords: null,
   };
 
-  constructor(props) {
+  mapState = {
+    center: [55.75, 37.57],
+    zoom: 9,
+  };
+
+  constructor (props) {
     super(props);
     this.mapRef = React.createRef();
+  }
+
+  componentDidMount () {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.warn('geolocation ok');
+        const { latitude, longitude } = position.coords;
+
+        this.setState({
+          currentCoords: [latitude, longitude],
+        });
+
+        console.warn(this.state.currentCoords);
+      },
+      (...args) => {
+        console.warn('geolocation error', ...args);
+      },
+    )
   }
 
   render () {
     return (
       <React.Fragment>
         <YMapsContainer>
-          <YMap defaultState={ this.mapState } instanceRef={ this.mapRef } width="100%" height="100%" />
+          <YMap defaultState={this.state.mapOptions} instanceRef={this.mapRef} width="100%" height="100%">
+            <Placemark geometry={this.state.mapOptions.center}/>
+          </YMap>
         </YMapsContainer>
       </React.Fragment>
     );
